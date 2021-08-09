af9efab0b2fb027da3b2baf6c110d7837bf77cc6
saranyamaity <saranyamaity2000@gmail.com>
2021-08-09 08:31:04 +0530

    just before endblock - 02

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
-from flask import Flask  # Flask class
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Saranya Maity',
+        'title': 'How to Excel in Competitive Programming',
+        'content': 'Go to my Github profile github.com/saranyamaity2000',
+        'date': 'Aug 9, 2021'
+    },
+    {
+        'author': 'Swagato Bag',
+        'title': 'Placed in HRC!',
+        'content': 'I am very glad to say, after a lot work experience finally placed in HRC',
+        'date': 'Aug 20, 2021'
+    }
+]
 
 
 @app.route("/")  # these are decorators
 @app.route("/home")
 def hello():
-    return "<h1> *-HOME PAGE-* </h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "ABOUT PAGE"
+    return render_template('about.html' , title='about')
 
 
 # most of the time we will be using
