3b87ebf9bce3e10f7257055b34298061a5fb92fa
saranyamaity <saranyamaity2000@gmail.com>
2021-08-11 12:17:06 +0530

    improvements done

--- main.py
+++ main.py
@@ -49,9 +49,15 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Log in Unsuccessful! Kindly check your Email and Password.', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
