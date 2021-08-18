ddf63e504c0aa9e089f61f1b96bca84500518b7d
saranyamaity <saranyamaity2000@gmail.com>
2021-08-18 14:43:21 +0530

    prfile pic updation cpmplete

--- main/routes.py
+++ main/routes.py
@@ -1,4 +1,5 @@
 import os
+from PIL import Image
 import secrets
 from flask import render_template, url_for, flash, redirect, request
 from flask_login import login_user, current_user, logout_user, login_required
@@ -80,7 +81,12 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-    form_picture.save(picture_path)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+
+    i.save(picture_path)
 
     return picture_fn
 
